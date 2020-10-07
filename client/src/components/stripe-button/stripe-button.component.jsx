cd974668176b68bf33c2ce0d2977a2a66d4f2103
Jovan12345 <jovanj@gecko.mk>
2020-10-07 12:35:40 +0200

    separating backend from frontend

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51HWhuuB3dwdZ1MyzyAZrEzYftcXWJV6qvYy1AbJ2DDFlVNBTSi5IfXdiypOas5jkMcFqPgDgm8LmnwAs5kMbnJhg005wZSEzhZ';
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successfull')
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment successful')
+        }).catch( error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert('There was an issue with your payment. Please make sure you use the provided credit card.')
+        })
     }
 
     return (
